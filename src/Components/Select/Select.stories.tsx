6530b591b54af22a4dcfc7ef94b1ccbb8ed6c6e4
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-10-12 20:35:22 +0300

    Сделал простенький псевдоselect

--- src/Components/Select/Select.stories.tsx
+++ src/Components/Select/Select.stories.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import Select from "./Select";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Select',
     component: Select
 }
 
-export const MySelect = () => <Select valueId={3} onChange={() => console.log('Заглушка')}
-                                      items={[
-                                          {title: 'Julia', value: 1},
-                                          {title: 'Liza', value: 2},
-                                          {title: 'Artem', value: 3},
-                                          {title: 'Viktor', value: 4},
-                                      ]}/>
+export const MySelect1 = () =>{
+    const [value,setValue] = useState(1)
+
+    return <Select valueId={value} onChange={setValue}
+                   items={[
+                       {title: 'Julia', value: 1},
+                       {title: 'Liza', value: 2},
+                       {title: 'Artem', value: 3},
+                       {title: 'Viktor', value: 4},
+                   ]}/>
+}
+
+export const MySelect2 = () => <Select valueId={2} onChange={() => console.log('Заглушка')}
+                                       items={[
+                                           {title: 'Julia', value: 1},
+                                           {title: 'Liza', value: 2},
+                                           {title: 'Artem', value: 3},
+                                           {title: 'Viktor', value: 4},
+                                       ]}/>
+
+export const MySelect3 = () => <Select valueId={3} onChange={() => console.log('Заглушка')}
+                                       items={[
+                                           {title: 'Julia', value: 1},
+                                           {title: 'Liza', value: 2},
+                                           {title: 'Artem', value: 3},
+                                           {title: 'Viktor', value: 4},
+                                       ]}/>
+
+export const MySelect4 = () => <Select valueId={4} onChange={() => console.log('Заглушка')}
+                                       items={[
+                                           {title: 'Julia', value: 1},
+                                           {title: 'Liza', value: 2},
+                                           {title: 'Artem', value: 3},
+                                           {title: 'Viktor', value: 4},
+                                       ]}/>
+
+export const UndefinedSelect = () => <Select valueId={5} onChange={() => console.log('Заглушка')}
+                                       items={[
+                                           {title: 'Julia', value: 1},
+                                           {title: 'Liza', value: 2},
+                                           {title: 'Artem', value: 3},
+                                           {title: 'Viktor', value: 4},
+                                       ]}/>
--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, FocusEvent} from 'react';
 import '../../App.css';
 
 type ItemType = {
@@ -12,19 +12,23 @@ type SelectPropsType = {
 }
 
 function Select(props: SelectPropsType) {
-    const [collapsed, setCollapsed] = useState(true)
-
-    const styleSelect = props.items.find( f=> f.value === props.valueId ) ? 'styleSelect' : 'error'
+    const [collapsed, setCollapsed] = useState(false)
 
     const element = props.items.find(f => f.value === props.valueId)
-    const arrayElements = props.items.map((f, index) => <div key={index}
-                                                             className={f.value === props.valueId ? 'styleSelect' : 'error'}> {f.title} </div>)
+    const arrayElements = props.items.map((f, index) => {
+
+        return <div key={index} className={f.value === props.valueId ? "styleSelect" : 'hover'} onClick={()=>{props.onChange(f.value); setCollapsed(false)} }> {f.title} </div>
+    })
 
     const onClickHandler = () => setCollapsed(!collapsed)
-
+    const onBlurHandler = (e:FocusEvent<HTMLInputElement>) => {
+       if( e.target.tagName === 'div') {
+           return alert('dada')
+       }
+    }
     return (
         <div className={'select'}>
-            <div className={'element'} onClick={onClickHandler}> {element && element.title} </div>
+            <div className={'element'} onClick={onClickHandler} onBlur={onBlurHandler} > {element && element.title} </div>
             {collapsed && arrayElements}
         </div>
     );
