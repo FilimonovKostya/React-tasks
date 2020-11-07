d8504d215d2f7135b09fd4b8029d162496810a82
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-07 12:28:28 +0300

    useMemo сделать потом домашку

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -77,11 +77,19 @@ export const HelpReactMemo = () => {
 }
 
 
-export const Users = (props: {users: Array<string>}) => <div>
-    
-</div>
+export const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('USERS SECRET')
+
+    return <div>
+        {props.users.map((u, i) => <div key={i}> {u} </div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
 
 export const HelpsToReactMemo = () => {
+    console.log('HELPSTOREACTMEMO')
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
 
@@ -93,13 +101,14 @@ export const HelpsToReactMemo = () => {
 
     const addUser = () => {
         const newUsers = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUsers)
     }
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
         <button onClick={() => addUser()}>add user</button>
         {counter}
-        <Users  users={newArray}/>
+        <Users users={newArray}/>
     </>
 
 }
