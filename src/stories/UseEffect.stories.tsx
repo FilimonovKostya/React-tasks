4071cce22ff66088dc50cd2a3f3c986c22a11b5c
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-08 22:55:50 +0300

    refactoring

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,12 +10,48 @@ export const SimpleExam1 = () => {
     console.log('SimpleExam1')
 
     useEffect(() => {
-        console.log('Use effect')
+        console.log('Use effect every render')
+        document.title = counter.toString()
     })
+
+    useEffect(() => {
+        console.log('Use effect only one first')
+        document.title = counter.toString()
+
+    })
+    useEffect(() => {
+        console.log('Use effect evere render when counter change')
+        document.title = counter.toString()
+
+    },[counter])
 
     return <>
         <button onClick={() => setCounter(counter + 1)}> +</button>
-      Hello,  {counter}
+        Hello, {counter}
     </>
 }
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(0)
+    const [fake, setFake] = useState(0)
+
+    console.log('SetTimeoutExample')
+
+    // useEffect(() => {
+    //     console.log('useEffect every render')
+    //     document.title = counter.toString()
+    // })
+
+
+    setTimeout(() => {
+        setInterval(() => {
+            setCounter(state => new Date().getDate())
+        })
+    },1000)
+
+    return <>
+       Time : {counter}
+    </>
+}
+
 
