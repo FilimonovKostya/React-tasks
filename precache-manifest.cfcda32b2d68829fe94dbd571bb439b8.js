deda1b08664ed5a4389b5a9ced66192b1f4750e4
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-10 08:20:00 +0300

    Updates

---
+++ precache-manifest.cfcda32b2d68829fe94dbd571bb439b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091b10548929deac6d7d68eefec54ce8",
+    "url": "/React-tasks/index.html"
+  },
+  {
+    "revision": "c7c3d3ab6807c9cd3b4b",
+    "url": "/React-tasks/static/css/main.335a3ef0.chunk.css"
+  },
+  {
+    "revision": "cc166eb41e4d2ddbff3f",
+    "url": "/React-tasks/static/js/2.6785cd20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-tasks/static/js/2.6785cd20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c3d3ab6807c9cd3b4b",
+    "url": "/React-tasks/static/js/main.dc33e228.chunk.js"
+  },
+  {
+    "revision": "cefb6db35d0df83bbedd",
+    "url": "/React-tasks/static/js/runtime-main.c703530c.js"
+  }
+]);
