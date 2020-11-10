cd3c1352cd61d616159db27127063d776ef80e5a
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-10 14:49:32 +0300

    сброс useEffect , зачистка 24 урок

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -23,7 +23,7 @@ export const SimpleExam1 = () => {
         console.log('Use effect evere render when counter change')
         document.title = counter.toString()
 
-    },[counter])
+    }, [counter])
 
     return <>
         <button onClick={() => setCounter(counter + 1)}> +</button>
@@ -33,7 +33,7 @@ export const SimpleExam1 = () => {
 
 export const SetTimeoutExample = () => {
 
-    let [time,setTime] = useState(0)
+    let [time, setTime] = useState(0)
 
     console.log('SetTimeoutExample')
 
@@ -47,13 +47,53 @@ export const SetTimeoutExample = () => {
         setInterval(() => {
             let dateTime = new Date()
             setTime(dateTime.getDate())
-            console.log(dateTime)
         })
-    },1000)
+    }, 1000)
 
     return <>
-       Time : {time}
+        Time : {time}
     </>
 }
 
 
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExpample')
+
+    useEffect(() => {
+        console.log('Эффект выполнился')
+
+        return () => {
+            console.log('Reset EFFECT')
+        }
+    }, [])
+
+    return <div>
+        Hello, counter : {counter}
+        <button onClick={() => setCounter(counter + 1)}> +</button>
+    </div>
+}
+
+export const KeyTrackerExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('Componend rendered with ' + text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+
+    }, [text])
+
+    return <div>
+        Typed text: {text}
+    </div>
+}
